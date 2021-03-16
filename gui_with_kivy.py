d4e3d3f7cd02976619f6be2c514a3f4239331353
DATA <nils.hartholt@gmail.com>
2021-03-16 18:26:28 +0100

    changes

--- gui_with_kivy.py
+++ gui_with_kivy.py
@@ -2,9 +2,12 @@ import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 
+class Login(Widget):
+    pass
+
 class TestApp(App):
     def build(self):
-        return Button(text="Test")
+        return Login()
 
 if __name__ == "__main__":
     TestApp().run()
