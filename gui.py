03d43b99107133d359ba81fb7da7f5b1fb459c5a
DATA <nils.hartholt@gmail.com>
2021-03-16 17:10:01 +0100

    einzelne Verbesserungen am Code

--- gui.py
+++ gui.py
@@ -3,8 +3,18 @@ import tkinter as tk
 class main_window():
     def __init__(self):
         self.main = tk.Tk()
+        self.close_button()
         self.main.mainloop()
 
+    def close_button(self):
+        button = tk.Button(self.main)
+        button["text"] = "Fenster schließen"
+        button["command"] = self.__del__
+        button.pack()
 
-    def ende(self):
+    def __del__(self):
         self.main.destroy()
+
+
+main_window()
+        
