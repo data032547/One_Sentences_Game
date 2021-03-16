4b1d980be34b1f3d369cffb72ad26501ac1b0814
DATA <nils.hartholt@gmail.com>
2021-03-16 17:34:07 +0100

    kivy Test

--- gui_with_kivy.py
+++ gui_with_kivy.py
@@ -1,3 +1,11 @@
 import kivy
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.button import Button
+
+class TestApp(App):
+    def build(self):
+        return Button(text="Test")
+
+if __name__ == "__main__":
+    TestApp().run()
