e2d8af32525544055170d3d20f12e2d4ae298f81
DATA <nils.hartholt@gmail.com>
2021-03-17 09:02:56 +0100

    explain

---
+++ testversion_gui_with_kivy.py
@@ -0,0 +1,30 @@
+"""
+Diese Datei dient zur Übung mit kivy
+Verwendete Tools:
+- kivy
+- time
+Autor:
+- Nils
+
+"""
+#Importieren der benötigten Libs
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from time import sleep 
+
+#Standart App Code
+class StandartApp(App):
+    def build(self):
+        return Button(text="Dies ist ein Standartfenster")
+
+#Loginfeld Code
+class Login(Widget):
+    pass
+class TestApp(App):
+    def build(self):
+        return Login()
+
+#führt x-Objekt bei Test aus
+if __name__ == "__main__":
+    StandartApp().run()
