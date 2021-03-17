804d33501f18c9cc0800140f2a4477aa5724abad
DATA <nils.hartholt@gmail.com>
2021-03-17 08:54:06 +0100

    Update gui_with_kivy.py

--- gui_with_kivy.py
+++ gui_with_kivy.py
@@ -1,10 +1,11 @@
-import kivy
 from kivy.app import App
+from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+
+from time import sleep 
 
 class Login(Widget):
     pass
-
 class TestApp(App):
     def build(self):
         return Login()
