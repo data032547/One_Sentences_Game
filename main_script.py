493263d148af896c51e68f07aa049c1ff3f62caa
DATA <nils.hartholt@gmail.com>
2021-03-20 17:52:09 +0100

    Update: remove holes

--- main_script.py
+++ main_script.py
@@ -17,7 +17,6 @@ from kivy.uix.textinput import TextInput
 from kivy.config import Config
 from kivy.uix.popup import Popup
 from kivy.core.window import Window
-
 from time import sleep 
 
 #Standart App Code
@@ -26,9 +25,7 @@ class StandartApp(App):
         return Button(text="Dies ist ein Standartfenster")
 
 # Vorbereitung eines Modules für's Spiel
-
 """ Optik ist noch deutlich ausbaufähig! """
-
 class MyGridLayout(GridLayout):
     def __init__(self,liste=[]):
         self.liste = liste
@@ -36,53 +33,41 @@ class MyGridLayout(GridLayout):
         #Constructor Call
         super(MyGridLayout, self).__init__()
         self.fenster_laden()
-        
-
     def fenster_laden(self):
-        
         # Erklärung aufrufen
         erklärung = Button(text="Spielanleitung")
         erklärung.bind(on_press=self.erklärung)
         self.add_widget(erklärung)
-        
         # setzte Säulen
         self.cols = 1
-
         # setzte letzen Satz Label
         self.letzter_satz = Label(text="None", font_size = 32)
         self.add_widget(self.letzter_satz)
-
         # setzte Input
         self.satz = TextInput(multiline=True,text="Satz hier eingeben...",font_size=32)
         self.add_widget(self.satz)
-
         # setzte Button absenden
         absenden = Button(text="absenden", font_size = 32)
         absenden.bind(on_press=self.next)
         self.add_widget(absenden)
-
         # setzte Button Spiel beenden
         beenden = Button(text="Spiel beenden", font_size = 22)
         beenden.bind(on_press=self.ende)
         self.add_widget(beenden)
-
     def next(self,instance):
         self.liste.append(self.satz.text)
         #clear
         self.letzter_satz.text = self.satz.text
         self.satz.text = "Satz hier eingeben..."
-
     def ende(self,instance):
         print(self.liste)
         MyApp().stop()
-
     def erklärung(self,instance):
         # Erklärung Popup
         popup = Popup(title='Spielanleitung',
         content=Label(text='Yeet'),
         size_hint=(None, None), size=(400, 400))
         popup.open()
-
 class MyApp(App):
     def build(self):
         return MyGridLayout()
