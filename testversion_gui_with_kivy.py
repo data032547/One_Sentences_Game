f5e68a8aee80ba34eef2ff69b6b1efaa04cded59
DATA <nils.hartholt@gmail.com>
2021-03-20 16:59:32 +0100

    update

--- testversion_gui_with_kivy.py
+++ testversion_gui_with_kivy.py
@@ -23,7 +23,8 @@ class StandartApp(App):
 
 # Vorbereitung eines Modules für's Spiel
 class MyGridLayout(GridLayout):
-    def __init__(self):
+    def __init__(self,liste=[]):
+        self.liste = liste
 
         #Constructor Call
         super(MyGridLayout, self).__init__()
@@ -33,22 +34,35 @@ class MyGridLayout(GridLayout):
     def fenster_laden(self):
         # setzte Säulen
         self.cols = 1
+
         # setzte letzen Satz Label
         self.letzter_satz = Label(text="None", font_size = 32)
         self.add_widget(self.letzter_satz)
+
         # setzte Input
         self.satz = TextInput(multiline=True,text="Satz hier eingeben...",font_size=32)
         self.add_widget(self.satz)
-        # setzte Button
+
+        # setzte Button absenden
         absenden = Button(text="absenden", font_size = 32)
         absenden.bind(on_press=self.next)
         self.add_widget(absenden)
 
+        # setzte Button Spiel beenden
+        beenden = Button(text="Spiel beenden", font_size = 22)
+        beenden.bind(on_press=self.ende)
+        self.add_widget(beenden)
+
+
     def next(self,instance):
-        satz = self.satz.text
+        self.liste = self.liste + self.satz.text
+
         #clear
-        self.letzter_satz.text = satz
+        self.letzter_satz.text = self.satz.text
         self.satz.text = "Satz hier eingeben..."
+
+    def ende(self):
+        print(self.liste)
 
 class Test(App):
     def build(self):
