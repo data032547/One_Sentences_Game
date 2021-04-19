d81be125029443e9a8c5ad41d5bb98d5c7b570dd
Typ <80766276+derjungemitdemzweikilobrain@users.noreply.github.com>
2021-04-19 20:07:33 +0200

    Produkt in PDF Erstansatz
    is pip falsch, was ist hier los, ahhhhhhhhhhhh

---
+++ Tim_PDF_stuff.py
@@ -0,0 +1,33 @@
+pip install reportlab
+
+from reportlab.lib.enums import TA_JUSTIFY, TA_CENTER
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.units import inch
+
+document = []
+
+def addTitle(doc):
+    doc.append(Spacer(1, 20))
+    doc.append(Paragraph('Ein_Satz_Spiel', ParagraphStyle(name='Yeat',
+                                                          fontFamily='Helvetica',
+                                                          fontSize=36,
+                                                          alignment=TA_CENTER)))
+    doc.append(Spacer(1, 50))
+    return doc
+
+def addParagraphs(doc):
+    doc.append(Spacer(1, 20))
+    doc.append(Paragraph('hier_strings_von_nils_einfügen', ParagraphStyle(name='Yoot',
+                                                          fontFamily='Helvetica',
+                                                          fontSize=20,
+                                                          alignment=TA_JUSTIFY)))
+    doc.append(Spacer(1, 20))
+    return doc
+
+    document = addTitle(document)
+
+SimpleDocTemplate('One_Sentence_Game.pdf', pagesize=letter,
+                  rightMargin=12, leftMargin=12,
+                  topMargin=12, bottomMargin=6).build(addParagraphs(document))
